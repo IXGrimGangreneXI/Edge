deb2ac974558170c52c40f37dc549be265589ce7
Sky Swimmer <stefaneekhof@aerialworks.ddns.net>
2023-08-20 16:28:45 +0200

    More Edge API functionality

--- src/gameplayapi/src/main/java/org/asf/edge/gameplayapi/http/handlers/edgespecific/EdgeApiService.java
+++ src/gameplayapi/src/main/java/org/asf/edge/gameplayapi/http/handlers/edgespecific/EdgeApiService.java
@@ -56,7 +56,6 @@ public class EdgeApiService extends EdgeWebService<EdgeGameplayApiServer> {
 	}
 
 	// TODO: profile and account updating
-	// TODO: downloads
 
 	@Function(allowedMethods = { "POST" })
 	public FunctionResult getProfileByID(FunctionInfo func) throws IOException {
@@ -597,6 +596,38 @@ public class EdgeApiService extends EdgeWebService<EdgeGameplayApiServer> {
 		return ok("text/json", resp.toString());
 	}
 
+	@Function
+	public FunctionResult deleteAccount(FunctionInfo func) throws IOException {
+		// Save details
+
+		// Check authentication
+		if (!func.getRequest().hasHeader("Authorization")
+				|| !func.getRequest().getHeader("Authorization").getValue().toLowerCase().startsWith("bearer ")) {
+			// Error
+			return response(401, "Unauthorized", "text/json", "{\"error\":\"Missing authorization token\"}");
+		}
+
+		// Read token
+		String token = func.getRequest().getHeader("Authorization").getValue().substring("bearer ".length());
+		SessionToken tkn = new SessionToken();
+		TokenParseResult res = tkn.parseToken(token);
+		AccountObject acc = tkn.account;
+		if (res != TokenParseResult.SUCCESS) {
+			// Error
+			return response(401, "Unauthorized", "text/json", "{\"error\":\"Authorization token invalid\"}");
+		}
+
+		// Delete
+		acc.deleteAccount();
+
+		// Build response
+		JsonObject resp = new JsonObject();
+		resp.addProperty("deleted", true);
+
+		// Return
+		return ok("text/json", resp.toString());
+	}
+
 	@Function(allowedMethods = { "POST" })
 	public FunctionResult authenticateSave(FunctionInfo func) throws IOException {
 		// Authenticate for save
@@ -690,6 +721,192 @@ public class EdgeApiService extends EdgeWebService<EdgeGameplayApiServer> {
 			rankData.add(rObj);
 		}
 		resp.add("ranks", rankData);
+
+		// Return
+		return ok("text/json", resp.toString());
+	}
+
+	@Function(allowedMethods = { "POST" })
+	public FunctionResult createSave(FunctionInfo func) throws IOException {
+		// Save details
+
+		// Check authentication
+		if (!func.getRequest().hasHeader("Authorization")
+				|| !func.getRequest().getHeader("Authorization").getValue().toLowerCase().startsWith("bearer ")) {
+			// Error
+			return response(401, "Unauthorized", "text/json", "{\"error\":\"Missing authorization token\"}");
+		}
+
+		// Read token
+		String token = func.getRequest().getHeader("Authorization").getValue().substring("bearer ".length());
+		SessionToken tkn = new SessionToken();
+		TokenParseResult res = tkn.parseToken(token);
+		AccountObject acc = tkn.account;
+		if (res != TokenParseResult.SUCCESS) {
+			// Error
+			return response(401, "Unauthorized", "text/json", "{\"error\":\"Authorization token invalid\"}");
+		}
+
+		// Load request
+		JsonObject request;
+		try {
+			// Parse and check
+			request = JsonParser.parseString(func.getRequest().getRequestBodyAsString()).getAsJsonObject();
+			if (!request.has("saveUsername"))
+				throw new IOException();
+		} catch (Exception e) {
+			// Bad request
+			return response(400, "Bad request", "text/json",
+					"{\"error\":\"Invalid request body, expecting a JSON with a 'saveUsername' field\"}");
+		}
+
+		// Find account manager
+		AccountManager manager = AccountManager.getInstance();
+
+		// Pull save username
+		String saveUsername = request.get("saveUsername").getAsString();
+
+		// Check validity
+		if (!manager.isValidUsername(saveUsername)) {
+			// Invalid name
+			return response(400, "Bad request", "text/json", "{\"error\":\"invalid_username\"}");
+		}
+
+		// Check filters
+		if (TextFilterService.getInstance().isFiltered(saveUsername, true)) {
+			// Invalid name
+			return response(400, "Bad request", "text/json", "{\"error\":\"imappropriate_username\"}");
+		}
+
+		// Check if in use
+		boolean inUse = false;
+		if (!acc.getUsername().equalsIgnoreCase(saveUsername) && manager.isUsernameTaken(saveUsername)) {
+			inUse = true;
+		} else {
+			// Check if in use by any saves
+			AccountObject accF = acc;
+			if (Stream.of(accF.getSaveIDs()).map(t -> accF.getSave(t)).anyMatch(t -> {
+				return t.getUsername().equalsIgnoreCase(saveUsername);
+			})) {
+				inUse = true;
+			}
+		}
+		if (inUse) {
+			// Invalid name
+			return response(400, "Bad request", "text/json", "{\"error\":\"username_in_use\"}");
+		}
+
+		// Create save
+		AccountSaveContainer save = acc.createSave(saveUsername);
+		if (save == null) {
+			// Error
+			return response(500, "Internal server error", "text/json", "{\"error\":\"save_creation_failure\"}");
+		}
+
+		// Build save-specific token
+		tkn = new SessionToken();
+		tkn.accountID = acc.getAccountID();
+		tkn.saveID = save.getSaveID();
+		tkn.lastLoginTime = acc.getLastLoginTime();
+		tkn.capabilities = new String[] { "api", "gp" };
+
+		// Log
+		getServerInstance().getLogger().info("Viking created for account " + acc.getAccountID() + ": selected viking '"
+				+ save.getUsername() + "' (ID " + save.getSaveID() + ") from API.");
+		save.getAccount().ping(true);
+
+		// Encode
+		token = tkn.toTokenString();
+
+		// Build response
+		JsonObject resp = new JsonObject();
+		resp.addProperty("token", token);
+		resp.addProperty("accountId", acc.getAccountID());
+		resp.addProperty("accountUsername", acc.getUsername());
+		resp.addProperty("saveId", save.getSaveID());
+		resp.addProperty("saveUsername", save.getUsername());
+		resp.addProperty("lastLoginTime", acc.getLastLoginTime());
+		resp.addProperty("registrationTime", acc.getRegistrationTimestamp());
+		resp.addProperty("saveCreationTime", save.getCreationTime());
+		resp.addProperty("multiplayerEnabled", acc.isMultiplayerEnabled());
+		resp.addProperty("chatEnabled", acc.isChatEnabled());
+		resp.addProperty("strictChat", acc.isStrictChatFilterEnabled());
+
+		// Find avatar
+		JsonElement ele = save.getSaveData().getEntry("avatar");
+		if (ele != null) {
+			resp.add("avatar", ele);
+		}
+
+		// Rank data
+		JsonArray rankData = new JsonArray();
+		for (EntityRankInfo rank : AchievementManager.getInstance().getRanks(save, save.getSaveID())) {
+			if (rank.getRank() == null)
+				continue;
+			JsonObject rObj = new JsonObject();
+			rObj.addProperty("rankID", rank.getRank().getID());
+			rObj.addProperty("rankGlobalID", rank.getRank().getGlobalID());
+			rObj.addProperty("pointTypeID", rank.getRank().getPointTypeID());
+			rObj.addProperty("level", AchievementManager.getInstance().getRankIndex(rank.getRank()) + 1);
+			rObj.addProperty("points", rank.getTotalScore());
+			rankData.add(rObj);
+		}
+		resp.add("ranks", rankData);
+
+		// Return
+		return ok("text/json", resp.toString());
+	}
+
+	@Function(allowedMethods = { "POST" })
+	public FunctionResult deleteSave(FunctionInfo func) throws IOException {
+		// Save details
+
+		// Check authentication
+		if (!func.getRequest().hasHeader("Authorization")
+				|| !func.getRequest().getHeader("Authorization").getValue().toLowerCase().startsWith("bearer ")) {
+			// Error
+			return response(401, "Unauthorized", "text/json", "{\"error\":\"Missing authorization token\"}");
+		}
+
+		// Read token
+		String token = func.getRequest().getHeader("Authorization").getValue().substring("bearer ".length());
+		SessionToken tkn = new SessionToken();
+		TokenParseResult res = tkn.parseToken(token);
+		AccountObject acc = tkn.account;
+		if (res != TokenParseResult.SUCCESS) {
+			// Error
+			return response(401, "Unauthorized", "text/json", "{\"error\":\"Authorization token invalid\"}");
+		}
+
+		// Load request
+		JsonObject request;
+		try {
+			// Parse and check
+			request = JsonParser.parseString(func.getRequest().getRequestBodyAsString()).getAsJsonObject();
+			if (!request.has("saveID"))
+				throw new IOException();
+		} catch (Exception e) {
+			// Bad request
+			return response(400, "Bad request", "text/json",
+					"{\"error\":\"Invalid request body, expecting a JSON with a 'saveID' field\"}");
+		}
+
+		// Pull save ID
+		String saveID = request.get("saveID").getAsString();
+
+		// Locate save
+		AccountSaveContainer save = acc.getSave(saveID);
+		if (save == null) {
+			// Error
+			return response(404, "Not found", "text/json", "{\"error\":\"save_not_found\"}");
+		}
+
+		// Delete
+		save.deleteSave();
+
+		// Build response
+		JsonObject resp = new JsonObject();
+		resp.addProperty("deleted", true);
 
 		// Return
 		return ok("text/json", resp.toString());
